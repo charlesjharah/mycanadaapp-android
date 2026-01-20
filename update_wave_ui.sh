1655be9f22200b2ee19fa949fa9b73a26ae891da
Bukacee <itzbuka@gmail.com>
2026-01-20 01:21:41 +0100

    Add wavy border and adjust image height

--- update_wave_ui.sh
+++ update_wave_ui.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "Creating Wavy Border UI..."
+
+# 1. Create the Wave Vector Drawable
+# This shape mimics a wave and will be colored #fdfdfd (Background color)
+cat <<EOF > app/src/main/res/drawable/ic_wave_shape.xml
+<vector xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="1440dp"
+    android:height="320dp"
+    android:viewportWidth="1440"
+    android:viewportHeight="320">
+    <path
+        android:fillColor="#fdfdfd"
+        android:pathData="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,261.3C960,256,1056,224,1152,208C1248,192,1344,192,1392,192L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z" />
+</vector>
+EOF
+
+# 2. Update the Layout to include the Wave
+cat <<EOF > app/src/main/res/layout/item_onboarding_page.xml
 <?xml version="1.0" encoding="utf-8"?>
 <androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
@@ -58,3 +79,6 @@
     </LinearLayout>
 
 </androidx.constraintlayout.widget.ConstraintLayout>
+EOF
+
+echo "DONE! Wavy border applied."
